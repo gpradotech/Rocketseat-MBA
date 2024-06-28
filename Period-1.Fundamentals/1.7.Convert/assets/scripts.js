fe5567e5081adc1630dad1a7fb006d71e0ba3c24
contato.gprado@gmail.com <contato.gprado@gmail.com>
2024-06-28 18:26:22 -0300

    Identificando a moeda e obtendo o valor

--- Period-1.Fundamentals/1.7.Convert/assets/scripts.js
+++ Period-1.Fundamentals/1.7.Convert/assets/scripts.js
@@ -1,7 +1,37 @@
+// Cotação de moedas do dia
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+// obtendo os elementos do formulário
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
 
 // Manipulando o input amount para receber apenas números
 amount.addEventListener('input', () => {
   const hasCharacterRegex = /\D+/g
   amount.value = amount.value.replace(hasCharacterRegex, "");
 })
+
+// Capturando o evento de submit do formulário
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+}
+
+// Função para converter a moeda
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
+}
